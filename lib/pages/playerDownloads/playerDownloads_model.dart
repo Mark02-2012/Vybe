15b55c4b0652c0dbe8be4d1484e3c872917dd416
Mark02-2012 <Marchingegno02@protonmail.com>
2026-04-29 16:02:54 +0200

    Create PlayerDownloadsModel class with init and dispose

---
+++ lib/pages/playerDownloads/playerDownloads_model.dart
@@ -0,0 +1,20 @@
+import '/backend/schema/structs/index.dart';
+import '/flutter_flow/flutter_flow_audio_player.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import 'player_downloads_widget.dart' show PlayerDownloadsWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class PlayerDownloadsModel extends FlutterFlowModel<PlayerDownloadsWidget> {
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {}
+}
