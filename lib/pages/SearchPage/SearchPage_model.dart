8db421bc92a63437f3944c50d130d0e5bbab32e5
Mark02-2012 <Marchingegno02@protonmail.com>
2026-04-29 16:00:07 +0200

    Create SearchPageModel for state management
    Added SearchPageModel class with state management for SearchPage.

---
+++ lib/pages/SearchPage/SearchPage_model.dart
@@ -0,0 +1,32 @@
+import '/backend/api_requests/api_calls.dart';
+import '/flutter_flow/flutter_flow_icon_button.dart';
+import '/flutter_flow/flutter_flow_theme.dart';
+import '/flutter_flow/flutter_flow_util.dart';
+import '/flutter_flow/flutter_flow_widgets.dart';
+import 'dart:ui';
+import '/index.dart';
+import 'search_page_widget.dart' show SearchPageWidget;
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:simple_gradient_text/simple_gradient_text.dart';
+
+class SearchPageModel extends FlutterFlowModel<SearchPageWidget> {
+  ///  State fields for stateful widgets in this page.
+
+  // State field(s) for TextField widget.
+  FocusNode? textFieldFocusNode;
+  TextEditingController? textController;
+  String? Function(BuildContext, String?)? textControllerValidator;
+  // Stores action output result for [Backend Call - API (YouTubeSearch)] action in TextField widget.
+  ApiCallResponse? apiResult7wf;
+
+  @override
+  void initState(BuildContext context) {}
+
+  @override
+  void dispose() {
+    textFieldFocusNode?.dispose();
+    textController?.dispose();
+  }
+}
