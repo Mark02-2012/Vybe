33253700b5a3cc5a058dd85eb1617ba3902498ad
Mark02-2012 <Marchingegno02@protonmail.com>
2026-05-31 11:14:56 +0200

    Update index.dart

--- lib/backend/schema/structs/index.dart
+++ lib/backend/schema/structs/index.dart
@@ -1,1 +1,8 @@
 export 'canzone_locale_struct.dart';
+// Esporta tutte le pagine per gli import di FlutterFlow
+export 'pages/HomePage/home_page_widget.dart';
+export 'pages/SearchPage/search_page_widget.dart';
+export 'pages/player/player_widget.dart';
+export 'pages/Downloadspage/downloadspage_widget.dart';
+export 'pages/playerDownloads/player_downloads_widget.dart';
+export 'pages/settings/settings_widget.dart';
